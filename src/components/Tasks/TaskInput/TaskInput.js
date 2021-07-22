3c4098f47f44eb8201d77b1782bbfd9ee1265554
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-07-22 12:30:59 -0400

    Changes in the repo name

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,47 +1,19 @@
-import React, { useState } from "react";
-import Button from "../../UI/Button/Button";
-import styles from "./TaskInput.module.css";
-// import styled from "styled-components";
+import React, { useState } from 'react';
+import Button from '../../UI/Button/Button';
+import styles from './TaskInput.module.css';
 
-// const FormControl = styled.div`
-//   margin: 0.5rem 0;
-
-//   & label {
-//     font-weight: bold;
-//     display: block;
-//     margin-bottom: 0.5rem;
-//     color: ${(props) => (props.invalid ? "red" : "black")};
-//   }
-
-//   & input {
-//     display: block;
-//     width: 100%;
-//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-//     background: ${(props) => (props.invalid ? "#f7d6d6" : "transparent")};
-//     font: inherit;
-//     line-height: 1.5rem;
-//     padding: 0 0.25rem;
-//   }
-
-//   & input:focus {
-//     outline: none;
-//     background: #fad0ec;
-//     border-color: #8b005d;
-//   }
-// `;
-
-const TaskInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState("");
+const TaskInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const taskInputChangeHandler = (e) => {
+  const taskInputChangeHandler = e => {
     if (e.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(e.target.value);
   };
 
-  const formSubmitHandler = (e) => {
+  const formSubmitHandler = e => {
     e.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -53,12 +25,11 @@ const TaskInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div
-        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
-      >
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Tasks I need to do:</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <input type='text' onChange={taskInputChangeHandler} />
       </div>
-      <Button type="submit">Add Task</Button>
+      <Button type='submit'>Add Task</Button>
     </form>
   );
 };
